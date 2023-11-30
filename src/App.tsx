f9c908cb1236d7eed09bb59373806c736e036895
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2023-11-30 14:45:40 +0100

    affichage graphique

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { calculerHeure } from './heure';
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [lune, setLune] = useState(0);
+  let [terre, setTerre] = useState(0);
+  let [soleil, setSoleil] = useState(0);
+  let [time, setTime] = useState("");
+  const handleChangeLune = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('new value', e.target.value)
+    const update=async()=>{
+      setLune(parseInt(e.target.value));
+    }
+    update();
+  },[lune])
+  const handleChangeTerre = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('new value', e.target.value)
+    const update=async()=>{
+      setTerre(parseInt(e.target.value));
+    }
+    update();
+  },[terre])
+  const handleChangeSoleil = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('new value', e.target.value)
+    const update=async()=>{
+      setSoleil(parseInt(e.target.value));
+    }
+    update();
+  },[soleil])
+  const handleClick = useCallback(async()=>{
+    const jupiterTime = calculerHeure(lune,terre,soleil);
+    setTime(jupiterTime);
+  },[lune,terre,soleil,time])
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Cadran de la lune</p><input type='number' onChange={handleChangeLune}></input>
+        <p>Cadran de la terre</p><input type='number' onChange={handleChangeTerre}></input>
+        <p>Cadran du soleil</p><input type='number' onChange={handleChangeSoleil}></input>
+        <div><button id='btn' onClick={handleClick}>Valider</button></div>
+        <p>{time==="" ? "": "Zone de temps de Jupiter : "+time}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/__tests__/heure.test.ts
+++ src/__tests__/heure.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest'
-import { calculerHeure } from '../Heure';
+import { calculerHeure } from '../heure';
 
 describe("Tests calcul de l'heure", () => {
     describe("cadran de la lune = 1", () => {
@@ -10,24 +10,24 @@ describe("Tests calcul de l'heure", () => {
             expect(calculerHeure(1, 1, 2)).toBe("nuight");
         });
         test("lune=1, terre=2, soleil=1", () => {
-            expect(calculerHeure(1, 1, 2)).toBe("mortin");
+            expect(calculerHeure(1, 2, 1)).toBe("mortin");
         });
         test("lune=1, terre=2, soleil=2", () => {
-            expect(calculerHeure(1, 1, 2)).toBe("nuight");
+            expect(calculerHeure(1, 2, 2)).toBe("nuight");
         });
     })
     describe("cadran de la lune = 2", () => {
         test("lune=2, terre=1, soleil=1", () => {
-            expect(calculerHeure(1, 1, 1)).toBe("aprenoon");
+            expect(calculerHeure(2, 1, 1)).toBe("aprenoon");
         });
         test("lune=2, terre=1, soleil=2", () => {
-            expect(calculerHeure(1, 1, 2)).toBe("soirning");
+            expect(calculerHeure(2, 1, 2)).toBe("soirning");
         });
         test("lune=2, terre=2, soleil=1", () => {
-            expect(calculerHeure(1, 1, 2)).toBe("aprenoon");
+            expect(calculerHeure(2, 2, 1)).toBe("aprenoon");
         });
         test("lune=2, terre=2, soleil=2", () => {
-            expect(calculerHeure(1, 1, 2)).toBe("nuight");
+            expect(calculerHeure(2, 2, 2)).toBe("nuight");
         });
     })
 })
