0c3ab0818c489996642ad18394e274f8fe473d08
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2023-12-01 11:44:45 +0100

    fin du TP

--- full-tests/time.test.ts
+++ full-tests/time.test.ts
@@ -24,3 +24,50 @@ test('test terre=3', async ({ page }) => {
   await expect(page.locator('#errormsg')).toContainText("Vous devez entrer comme valeurs 1 ou 2");
   //await expect(page.getByText('Vous devez entrer comme')).toContainText("Vous devez entrer comme valeurs 1 ou 2");
 });
+
+test('test visibilité bouton soleil non rempli ou égal à 0', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.locator('#moon').click();
+  await page.locator('#moon').fill('1');
+  await page.locator('#earth').click();
+  await page.locator('#earth').fill('1');
+  await page.locator('#sun').click();
+  await page.locator('#sun').fill('0');
+  await expect(page.locator('#btn')).not.toBeVisible();
+});
+
+test('test visibilité bouton terre non remplie ou égale à 0', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.locator('#moon').click();
+  await page.locator('#moon').fill('1');
+  await page.locator('#earth').click();
+  await page.locator('#earth').fill('0');
+  await page.locator('#sun').click();
+  await page.locator('#sun').fill('1');
+  await expect(page.locator('#btn')).not.toBeVisible();
+});
+
+test('le texte de résultat ne doit pas apparaître quand on change la valeur dun cadran', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.locator('#moon').click();
+  await page.locator('#moon').fill('1');
+  await page.locator('#earth').click();
+  await page.locator('#earth').fill('1');
+  await page.locator('#sun').click();
+  await page.locator('#sun').fill('1');
+  await page.locator('#btn').click();
+  await page.locator('#moon').fill('2');
+  await expect(page.locator('#msg')).not.toBeVisible();
+});
+
+test('le texte de résultat apparait quand le bouton de calcul est cliqué', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.locator('#moon').click();
+  await page.locator('#moon').fill('1');
+  await page.locator('#earth').click();
+  await page.locator('#earth').fill('1');
+  await page.locator('#sun').click();
+  await page.locator('#sun').fill('1');
+  await page.locator('#btn').click();
+  await expect(page.locator('#msg')).toContainText("Zone de temps de Jupiter");
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -11,6 +11,9 @@ function App() {
   let [soleil, setSoleil] = useState(0);
   let [time, setTime] = useState("");
   let [error, setError] = useState(false);
+  let [previouslune, setPreviouslune] = useState(0);
+  let [previousterre, setPreviousterre] = useState(0);
+  let [previoussoleil, setPrevioussoleil] = useState(0);
   const handleChangeLune = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     console.log('new value', e.target.value)
     const update=async()=>{
@@ -37,8 +40,17 @@ function App() {
     const jupiterTime = calculerHeure(lune,terre,soleil);
     setTime(jupiterTime);
     }
+    setPreviouslune(lune);
+    setPreviousterre(terre);
+    setPrevioussoleil(soleil);
     
   },[lune,terre,soleil,time])
+
+  useEffect(()=> {
+    if(lune!==previouslune||terre!==previousterre||soleil!==previoussoleil) {
+      setTime("");
+    }
+  },[lune,terre,soleil,previouslune,previousterre,previoussoleil])
 
   return (
     <>
@@ -47,8 +59,8 @@ function App() {
         <p>Cadran de la lune</p><input type='number' id='moon' onChange={handleChangeLune}></input>
         <p>Cadran de la terre</p><input type='number' id='earth' onChange={handleChangeTerre}></input>
         <p>Cadran du soleil</p><input type='number' id='sun' onChange={handleChangeSoleil}></input>
-        <div><button id='btn' onClick={handleClick}>Valider</button></div>
-        <p>{time==="" ? "": "Zone de temps de Jupiter : "+time}</p>
+        <div>{lune!==0&&terre!==0&&soleil!==0 ? <button id='btn' onClick={handleClick}>Valider</button>:<div></div>}</div>
+        {time===""||!(lune!==0&&terre!==0&&soleil!==0) ? <p></p>: <p id='msg'>"Zone de temps de Jupiter : "+time</p>}
       </div>
     </>
   )
